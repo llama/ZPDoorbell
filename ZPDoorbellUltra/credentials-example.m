41568bb59ed6fa2a8f5a9a2a2a9b75b235efd742
Doug Feigelson <dougfeig@gmail.com>
2013-02-18 14:52:55 -0500

    documentation

--- ZPDoorbellUltra/credentials-example.m
+++ ZPDoorbellUltra/credentials-example.m
@@ -6,6 +6,9 @@
 //  Copyright (c) 2013 Doug Feigelson. All rights reserved.
 //
 
+// Copy this file to "credentials.m" and fill out the IPs, ports, and secret keys.
+// See readme for more details.
+
 static NSString* DOORBELL_URL = @"http://18.228.0.200:5000/doorbell?key=ASECRETKEY";
 static NSString* DELIVERY_URL = @"http://18.228.0.200:5000/doorbell?key=ASECRETKEY";
 static NSString* ARBITRARY_TEXT_URL = @"http://18.228.0.200:5000/doorbell?key=ASECRETKEY&txt=";
